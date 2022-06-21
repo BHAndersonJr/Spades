cc8a52f807eb8a645de3862062256e1615306d0a
Brian Heath Anderson Jr <45954219+BHAndersonJr@users.noreply.github.com>
2022-06-21 18:44:59 -0400

    Add files via upload
    Work on spades game

---
+++ Game.java
@@ -0,0 +1,13 @@
+
+public class Game {
+    public Deck deck;
+
+
+
+
+    public Game(){
+        this.deck = new Deck();
+    }
+
+
+}
---
+++ Player.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+
+public class Player {
+
+
+    public int playerNum;
+    public ArrayList<Card> hand;
+
+    public Player(int playerNumber){
+        //need to test
+        this.hand = new ArrayList<Card>();
+        this.playerNum = playerNumber;
+    }
+
+    public void addCardToHand(Card card){
+        //need to test
+        this.hand.add(card);
+    }
+
+    public void printHand(){
+        for(int i=0;i <this.hand.size()-1;i++){
+            //need to test
+            System.out.println(this.hand.get(i).getSuit() + this.hand.get(i).getValue());
+        }
+
+
+    }
+
+
+}
